d550e0da203412eee8e30019d503454c32afd396
Nguyen Vi Khang <20130287@st.hcmuaf.edu.vn>
2023-07-12 13:01:08 +0700

    create frame for default layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,12 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
   return (
     <>
-      <h1>Sidebar</h1>
+      <aside className={cx("sidebar")}>Sidebar</aside>
     </>
   );
 }
